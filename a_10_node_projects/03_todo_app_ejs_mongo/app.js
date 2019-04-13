96c3de84fab5c87f70ab6a37f9d67af00577b538
jason.gralinski <jgralinski@me.com>
2019-04-12 17:36:48 -0700

    mongo

---
+++ a_10_node_projects/03_todo_app_ejs_mongo/app.js
@@ -0,0 +1,33 @@
+// if no express gen ,save this base as a snippet
+
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+// Init app
+const app = express();
+
+// get mongo rolling
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/todoapp';
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+app.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3K');
+});
