9c244546d82ff684d7cd090a0f68a62e4d604067
jason.gralinski <jgralinski@me.com>
2019-06-24 17:54:26 -0700

    added rest table

--- colt_web_dev_camp/restart/30_y_camp_data/04_from_scratch/app.js
+++ colt_web_dev_camp/restart/30_y_camp_data/04_from_scratch/app.js
@@ -3,22 +3,42 @@ const app = express();
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost/04_from_scratch');
+mongoose.connect('mongodb://localhost/04_from_scratch', {
+    useNewUrlParser: true
+});
+
+// SCEHMA
+let sushiSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+// MODEL
+let Sushi = mongoose.model('Sushi', sushiSchema);
 
 // LANDING
-app.get('/', (req, res, next)=>{
+app.get('/', (req, res, next) => {
     res.render('index');
 });
 
 // INDEX
-app.get('/sushi', (req, res, next)=>{
-    res.render('sushi');
-});
+app.get('/sushi', (req, res, next) => {
+    Sushi.find({}, (err, allSushi)=>{
+        if(err){
+            console.log(err);
+        } else {
+            res.render('index', {allSushi: allSushi});
+        }
+    })
+    });
 
 // NEW
 app.get('/sushi/new', (req, res, next) => {
@@ -27,18 +47,40 @@ app.get('/sushi/new', (req, res, next) => {
 
 // CREATE
 app.post('/sushi', (req, res, next) => {
-    res.render('sushi');
+    let name = req.body.name;
+    let image = req.body.image;
+    let description = req.body.description;
+    let newSushi = {
+        name: name,
+        image: image,
+        description: description
+    };
+    Sushi.create(newSushi, (err, newSushi) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect('sushi');
+        }
+    })
 });
 
 // SHOW
 app.get('/sushi/:id', (req, res, next) => {
-    res.render('sushi');
+    Sushi.findById(req.params.id, (err, foundSushi) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('show', {
+                foundSushi: foundSushi
+            });
+        }
+    })
 });
 
-app.get('*', (req, res, next)=>{
+app.get('*', (req, res, next) => {
     res.send('NoPe');
 });
 
-app.listen('3000', (req, res, next)=>{
+app.listen('3000', (req, res, next) => {
     console.log('all good on 3k')
 })
