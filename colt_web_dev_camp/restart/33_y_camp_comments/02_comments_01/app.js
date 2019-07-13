58f8f71062e104191b4f7cc760f95156d90532d0
jason.gralinski <jgralinski@me.com>
2019-07-12 18:12:38 -0700

    missing file

--- colt_web_dev_camp/restart/33_y_camp_comments/02_comments_01/app.js
+++ colt_web_dev_camp/restart/33_y_camp_comments/02_comments_01/app.js
@@ -120,27 +120,27 @@ app.get('/campgrounds/:id/comments/new', (req, res, next) => {
     })
 })
 
-app.post("/campgrounds/:id/comments", function (req, res) {
+app.post("/campgrounds/:id/comments", (req, res)=> {
     //lookup campground using ID
-    Campground.findById(req.params.id, function (err, campground) {
+    Campground.findById(req.params.id, (err, found_campground)=> {
         if (err) {
             console.log(err);
             res.redirect("/campgrounds");
         } else {
-            Comment.create(req.body.comment, function (err, comment) {
+            //create new comment
+            Comment.create(req.body.comment, (err, comment)=> {
                 if (err) {
                     console.log(err);
                 } else {
-                    campground.comments.push(comment);
-                    campground.save();
+                    //connect new comment to campground
+                    found_campground.comment.push(comment);
+                    found_campgrounds.save();
+                    //redirect campground show page
                     res.redirect('/campgrounds/' + campground._id);
                 }
             });
         }
     });
-    //create new comment
-    //connect new comment to campground
-    //redirect campground show page
 });
 
 
