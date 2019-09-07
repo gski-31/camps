ae1c4edfeaebc03864621009b28aea077f85a309
jason.gralinski <jgralinski@me.com>
2019-09-06 18:29:38 -0700

    arrays

--- modern_js/04_arrays/notes.js
+++ modern_js/04_arrays/notes.js
@@ -1,4 +1,6 @@
-const notes = ['note 01', 'note 02', 'note 03'];
+/* ---------- Basic Arrays ------------------ */
+
+// const notes = ['note 01', 'note 02', 'note 03'];
 
 // notes.push('new last item')
 // notes.pop() // remove last item
@@ -8,8 +10,51 @@ const notes = ['note 01', 'note 02', 'note 03'];
 
 // notes.splice(1,1);  // where to start, how many to remove
 // notes.splice(1, 0, 'insert me');  // where to start, how many to remove, insert soemthing
-notes.splice(1, 1, 'new 2nd item'); // similar to notes[1] = 'new 2nd item'
+// notes.splice(1, 1, 'new 2nd item'); // similar to notes[1] = 'new 2nd item'
 
-console.log(notes);
-console.log(notes.length);
-console.log(notes[notes.length-1]);
+// console.log(notes);
+// console.log(notes.length);
+// console.log(notes[notes.length-1]);
+
+/* ---------- forEach ------------------ */
+
+// const notes = ['note 01', 'note 02', 'note 03'];
+
+// notes.forEach((note, index)=>{
+//     console.log(`Item ${index+1}: ${note}`);
+// })
+
+/* ---------- for loop ------------------ */
+
+// basic info about
+
+/* ---------- search array ------------------ */
+
+// const notes = ['note 01', 'note 02', 'note 03'];
+
+// console.log(notes.indexOf('note 03'));
+// console.log(notes.indexOf('02')); // -1
+
+/* ---------- search array of objects ------------------ */
+
+const notes = [{
+    title: 'note 01',
+    body: 'this is the first note'
+}, {
+    title: 'note 02',
+    body: 'this is the second note'
+}, {
+    title: 'note 03',
+    body: 'this is the third note'
+}];
+
+const index = notes.findIndex((note, index)=>{
+    console.log(note)
+    return note.body === 'this is the third note'
+})
+
+console.log(index)
+
+
+
+
--- modern_js/04_arrays/todo.js
+++ modern_js/04_arrays/todo.js
@@ -12,3 +12,38 @@
 • add item to end
 • remove 1st item
 */
+
+// const todos = ['open vs code', 'find videos', 'code along', 'take test', 'go home']
+
+// todos.splice(2,1);
+// console.log(todos);
+
+// todos.push('end item');
+// console.log(todos);
+
+// todos.shift();
+// console.log(todos);
+
+/* CHALLENGE:
+print items in a list.*/
+
+// const todos = ['open vs code', 'find videos', 'code along', 'take test', 'go home']
+
+// todos.forEach((todo, index) => {
+//     console.log(`Item ${index + 1}: ${todo}`);
+// })
+
+/* CHALLENGE:
+do previous challeng ewith a for loop */
+
+// const todos = ['open vs code', 'find videos', 'code along', 'take test', 'go home']
+
+// for(let i = 0 ; i < todos.length ; i++){
+//     console.log(`Item ${i + 1}: ${todos[i]}`)
+// }
+
+
+
+
+
+
